f1dd1ee490d3ca78eb830f84e7cb2f5871fb2514
leslie-huang <lesliehuang@nyu.edu>
2019-06-11 17:00:37 -0400

    initial version working

--- scripts/convert_to_json.R
+++ scripts/convert_to_json.R
@@ -1,1 +1,16 @@
-js_filepath <- "https://github.com/leaflet-extras/leaflet-providers/blob/master/leaflet-providers.js"
+library(V8)
+library(jsonlite)
+
+ct <- v8()
+
+# create dummy Leaflet object
+ct$eval("var L = {TileLayer : {extend: function(){return {};}}, Util : {extend: function(){return {};}}, tileLayer : {}}")
+
+# Load providers.js file
+# curl from here https://unpkg.com/leaflet-providers
+providers_filepath <- "https://raw.githubusercontent.com/leaflet-extras/leaflet-providers/master/leaflet-providers.js"
+ct$source(providers_filepath)
+
+providers <- ct$eval("JSON.stringify(L.TileLayer.Provider.providers)")
+
+providers_ls <- fromJSON(providers)
