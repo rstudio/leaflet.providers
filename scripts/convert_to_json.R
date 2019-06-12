91572b15f814be3f3f22d0c6328b43b829d1b996
leslie-huang <lesliehuang@nyu.edu>
2019-06-12 09:27:54 -0400

    curl .js file and save data properly

--- scripts/convert_to_json.R
+++ scripts/convert_to_json.R
@@ -1,16 +1,27 @@
-library(V8)
-library(jsonlite)
-
-ct <- v8()
+ct <- V8::v8()
 
 # create dummy Leaflet object
 ct$eval("var L = {TileLayer : {extend: function(){return {};}}, Util : {extend: function(){return {};}}, tileLayer : {}}")
 
 # Load providers.js file
-# curl from here https://unpkg.com/leaflet-providers
-providers_filepath <- "https://raw.githubusercontent.com/leaflet-extras/leaflet-providers/master/leaflet-providers.js"
-ct$source(providers_filepath)
+req <- curl::curl("https://unpkg.com/leaflet-providers")
+ct$source(req)
 
-providers <- ct$eval("JSON.stringify(L.TileLayer.Provider.providers)")
+providers_json <- ct$eval("JSON.stringify(L.TileLayer.Provider.providers)")
 
-providers_ls <- fromJSON(providers)
+providers.details <- jsonlite::fromJSON(providers_json)
+
+variants <- purrr::map(providers.details, ~ names(.$variants))
+
+providers <- purrr::map(names(providers.details), function(provider) {
+  if (is.null(variants[[provider]])) {
+    provider
+  } else {
+    c(provider, stringr::str_c(provider,'.',variants[[provider]]))
+  }
+}) %>% purrr::flatten_chr()
+
+providers <- setNames(as.list(providers), providers)
+
+devtools::use_data(providers.details, overwrite = TRUE)
+devtools::use_data(providers, overwrite = TRUE)
