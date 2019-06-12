cde9771a4daabd21453f4f90aea49a8d8e9b8af4
leslie-huang <lesliehuang@nyu.edu>
2019-06-12 16:11:24 -0400

    document datasets

---
+++ R/providers.R
@@ -0,0 +1,8 @@
+#' Leaflet Providers.
+#'
+#' A dataset containing the names of leaflet providers.
+#'
+#' @format A list with 155 elements.
+#'
+#' @source \url{https://unpkg.com/leaflet-providers}
+"providers"
---
+++ R/providers.details.R
@@ -0,0 +1,13 @@
+#' Leaflet Providers' details.
+#'
+#' A dataset containing Leaflet Providers details.
+#'
+#' @format A nested list with 27 elements. Each element is a list containing
+#' \describe{
+#' \item{url}{source URL}
+#' \item{options}{options}
+#' \item{variants}{name variants, if any}
+#' }
+#'
+#' @source \url{https://unpkg.com/leaflet-providers}
+"providers.details"
