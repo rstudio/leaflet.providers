9eb181098917aebe0e597e975851a6eb13427cbc
leslie-huang <lesliehuang@nyu.edu>
2019-06-12 15:15:44 -0400

    /R function without dep on purrr/stringr

---
+++ R/get_current_providers.R
@@ -0,0 +1,36 @@
+#' Fetch leaflet providers from Leaflet.js.
+#' @export
+#'
+#' @return List of \code{providers}, \code{providers.details}
+#'
+#' @examples
+#' get_current_providers()
+#'
+
+get_current_providers <- function() {
+  ct <- V8::v8()
+
+  # create dummy Leaflet object
+  ct$eval("var L = {TileLayer : {extend: function(){return {};}}, Util : {extend: function(){return {};}}, tileLayer : {}}")
+
+  # Load providers.js file
+  ct$source("https://unpkg.com/leaflet-providers")
+
+  providers_json <- ct$eval("JSON.stringify(L.TileLayer.Provider.providers)")
+
+  providers.details <- jsonlite::fromJSON(providers_json)
+
+  variants <- lapply(providers.details, function(x){names(x$variants)})
+
+  providers <- lapply(names(providers.details), function(provider) {
+    if (is.null(variants[[provider]])) {
+      provider
+    } else {
+      c(provider, paste(provider, variants[[provider]], sep = "."))
+    }
+  }) %>% unlist()
+
+  providers <- setNames(as.list(providers), providers)
+
+  list(providers, providers.details)
+}
