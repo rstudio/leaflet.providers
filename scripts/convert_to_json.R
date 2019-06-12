7aebbf8e0d2edf4316803f13e5b8baaca78be74d
leslie-huang <lesliehuang@nyu.edu>
2019-06-12 11:48:33 -0400

    write .rda to /data

--- scripts/convert_to_json.R
+++ scripts/convert_to_json.R
@@ -1,1 +1,27 @@
-js_filepath <- "https://github.com/leaflet-extras/leaflet-providers/blob/master/leaflet-providers.js"
+ct <- V8::v8()
+
+# create dummy Leaflet object
+ct$eval("var L = {TileLayer : {extend: function(){return {};}}, Util : {extend: function(){return {};}}, tileLayer : {}}")
+
+# Load providers.js file
+req <- curl::curl("https://unpkg.com/leaflet-providers")
+ct$source(req)
+
+providers_json <- ct$eval("JSON.stringify(L.TileLayer.Provider.providers)")
+
+providers.details <- jsonlite::fromJSON(providers_json)
+
+variants <- purrr::map(providers.details, ~ names(.$variants))
+
+providers <- purrr::map(names(providers.details), function(provider) {
+  if (is.null(variants[[provider]])) {
+    provider
+  } else {
+    c(provider, stringr::str_c(provider,'.',variants[[provider]]))
+  }
+}) %>% purrr::flatten_chr()
+
+providers <- setNames(as.list(providers), providers)
+
+usethis::use_data(providers.details, overwrite = TRUE)
+usethis::use_data(providers, overwrite = TRUE)
