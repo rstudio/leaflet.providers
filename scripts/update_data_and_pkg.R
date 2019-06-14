37a3d27b361db7be1f45c89135500f7fd743fe40
leslie-huang <lesliehuang@nyu.edu>
2019-06-14 10:40:33 -0400

    change order of checks and automatically generated files

--- scripts/update_data_and_pkg.R
+++ scripts/update_data_and_pkg.R
@@ -16,8 +16,7 @@ devtools::test()
 desc::desc_set_version(todays_data$version_num)
 
 # Auto update NEWS.md
-lines <- readLines("NEWS.md")
-filecon <- file("NEWS.md", "wt")
+old_news <- readLines("NEWS.md")
 new_lines <- paste("# leaflet.providers",
                    todays_data$version_num,
                    "\n",
@@ -30,9 +29,10 @@ new_lines <- paste("# leaflet.providers",
                    "of leaflet.js",
                    "\n",
                    sep = " ")
-writeLines(new_lines, con = filecon)
-writeLines(lines, con = filecon)
-close(filecon)
+cat(file = "NEWS.md",
+    new_lines,
+    "\n",
+    paste0(old_news, collapse = "\n"))
 
 ## Final checks
 devtools::check()
@@ -46,21 +46,34 @@ spelling::spell_check_test(vignettes = TRUE, error = FALSE,
 # devtools::check_win_release()
 # devtools::check_win_devel()
 
-rhub::check_for_cran()
+rhub_output <- rhub::check_for_cran()
 
 # Generate revdep CRAN report (to include in cran-comments.md)
 revdep_report_results <- capture.output(revdepcheck::revdep_report_cran())
 
+cran_comments_msg <- "
+## Test environments
+* local OS X install, R 3.6.0
+* ubuntu 14.04 (on travis-ci), R 3.6.0, R 3.5.3, R devel
+* win-builder (devel and release)
+
+## R CMD check results
+
+0 errors | 0 warnings | 1 note
+
+* This is a new release.
+"
+
 # Auto-update cran-comments.md
-lines <- readLines("cran-comments.md")
-filecon <- file("cran-comments.md", "wt")
-new_lines <- paste("#", Sys.Date(),
-                   "\n\n",
-                   "This submission updates `providers` and `providers.details`.",
-                   "\n",
-                   sep = " ")
-writeLines(new_lines, con = filecon)
-writeLines(lines, con = filecon)
-writeLines("\n", con = filecon)
-writeLines(revdep_report_results, con = filecon)
-close(filecon)
+cat(file = "cran-comments.md",
+    "#", Sys.Date(),
+    "\n\n",
+    "This submission updates `providers` and `providers.details`.",
+    "\n\n",
+    cran_comments_msg,
+    "\n\n",
+    paste0(revdep_report_results, collapse = "\n"),
+    "\n",
+    paste0(rhub_output$cran_summary(), collapse = "\n")
+    )
+
