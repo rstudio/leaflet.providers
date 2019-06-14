e0e73040e882b10d7f25dbd0ee9923e36039d4ab
leslie-huang <lesliehuang@nyu.edu>
2019-06-14 07:19:06 -0400

    workaround lint style check

--- tests/testthat/test-providers.R
+++ tests/testthat/test-providers.R
@@ -2,4 +2,4 @@ context("Structure of providers")
 
 testthat::expect_gt(length(providers), 150)
 
-testthat::expect_true(!is.null(providers$OpenStreetMap))
+testthat::expect_true(!is.null(providers["OpenStreetMap"]))
--- tests/testthat/test-providersdetails.R
+++ tests/testthat/test-providersdetails.R
@@ -2,4 +2,4 @@ context("Structure of providers.details")
 
 testthat::expect_gt(length(providers.details), 25)
 
-testthat::expect_true(!is.null(providers.details$OpenStreetMap))
+testthat::expect_true(!is.null(providers.details["OpenStreetMap"]))
