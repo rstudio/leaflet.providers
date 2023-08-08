dd60dc1a70aca766b9c4b1712a2324c9ac239350
Barret Schloerke <barret@posit.co>
2023-08-08 08:34:00 -0400

    remove http replacments

--- R/get_current_providers.R
+++ R/get_current_providers.R
@@ -2,29 +2,6 @@ unpkg_url <- "https://unpkg.com/leaflet-providers"
 
 loaded_providers_env <- new.env()
 
-# this is to facilitate handling http / https protocols in leaflet
-# https://github.com/rstudio/rstudio/issues/2661
-# remove after RStudio 1.1 / next version of leaflet
-https_replace <- c(
-  "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  "//{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
-  "//tile.osm.ch/switzerland/{z}/{x}/{y}.png",
-  "//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
-  "//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-  "//tile.openstreetmap.bzh/br/{z}/{x}/{y}.png",
-  "//tiles.openseamap.org/seamark/{z}/{x}/{y}.png",
-  "//s3.amazonaws.com/te512.safecast.org/{z}/{x}/{y}.png",
-  "//{s}.tile.openstreetmap.se/hydda/{variant}/{z}/{x}/{y}.png",
-  "//stamen-tiles-{s}.a.ssl.fastly.net/{variant}/{z}/{x}/{y}{r}.{ext}",
-  "//stamen-tiles-{s}.a.ssl.fastly.net/{variant}/{z}/{x}/{y}.{ext}",
-  "//server.arcgisonline.com/ArcGIS/rest/services/{variant}/MapServer/tile/{z}/{y}/{x}",
-  "//{s}.{base}.maps.cit.api.here.com/maptile/2.1/",
-  "//maps{s}.wien.gv.at/basemap/{variant}/normal/google3857/{z}/{y}/{x}.{format}",
-  "//geodata.nationaalgeoregister.nl/tiles/service/wmts/{variant}/EPSG:3857/{z}/{x}/{y}.png",
-  "//geodata.nationaalgeoregister.nl/luchtfoto/rgb/wmts/1.0.0/2016_ortho25/EPSG:3857/{z}/{x}/{y}.png", # nolint
-  "//nls-{s}.tileserver.com/nls/{z}/{x}/{y}.jpg",
-  "//maps-{s}.onemap.sg/v3/{variant}/{z}/{x}/{y}.png"
-)
 
 #' Fetch leaflet providers from Leaflet.js.
 #' @export
@@ -53,18 +30,6 @@ get_providers <- function(version_num = NULL) {
   js_path <- paste0(unpkg_url, "@", version_num)
 
   tmp_js_lines <- paste0(readLines(js_path), collapse = "\n")
-
-  # this is to facilitate handling http / https protocols in leaflet
-  # https://github.com/rstudio/rstudio/issues/2661
-  # remove after RStudio 1.1 / next version of leaflet
-  for (url in https_replace) {
-    tmp_js_lines <- gsub(
-      paste0("https:", url),
-      url,
-      tmp_js_lines,
-      fixed = TRUE
-    )
-  }
 
   ct <- V8::v8()
 
