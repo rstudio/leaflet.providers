033b0795227e9fa7e3e8d0884ae5ba8674ae9b63
Garrick Aden-Buie <garrick@adenbuie.com>
2023-10-05 14:57:49 -0400

    feat: Update to leaflet-providers 2.0.0

--- tests/testthat/test-providers.R
+++
@@ -1,7 +0,0 @@
-context("Structure of providers_data")
-
-testthat::test_that("providers_data structure", {
-  testthat::expect_gt(length(providers_data), 150)
-
-  testthat::expect_true(!is.null(providers_data[["OpenStreetMap"]]))
-})
--- tests/testthat/test-providers_data.R
+++ tests/testthat/test-providers_data.R
@@ -1,6 +1,11 @@
-context("Structure of providers_details_data")
 
-testthat::test_that("providers.details structure", {
+testthat::test_that("providers_data structure", {
+  testthat::expect_gt(length(providers_data), 150)
+
+  testthat::expect_true(!is.null(providers_data[["OpenStreetMap"]]))
+})
+
+testthat::test_that("providers_details structure", {
   testthat::expect_gt(length(providers_details_data), 25)
 
   testthat::expect_true(!is.null(providers_details_data[["OpenStreetMap"]]))
@@ -11,8 +16,10 @@ testthat::test_that("providers.details structure", {
 
   testthat::expect_true(!is.null(providers_details_data[["OpenStreetMap"]][["variants"]]["Mapnik"]))
 
+  # 2023-10-05: leaflet-providers no longer uses subdomains for OpenStreetMap
+  # see https://github.com/leaflet-extras/leaflet-providers/pull/472
   testthat::expect_equal(
     providers_details_data[["OpenStreetMap"]][["url"]],
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
   )
 })
